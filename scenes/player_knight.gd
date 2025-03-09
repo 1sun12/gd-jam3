874620ee5396bf15ac1d08938ce99a246f8105c5
1sun12 <rro6@students.uwf.edu>
2025-03-09 13:15:52 -0600

    omni-dir-movement with anims WORKING on the knight player character

--- scenes/player_knight.gd
+++ scenes/player_knight.gd
@@ -6,6 +6,9 @@ extends CharacterBody2D
 const _SPEED: int = 100
 # determines how many hits the player can take before dying
 const _HEALTH: float = 100.0
+# holds last movement, to help display the right idle anim.
+# [1 = up], [2 = down], [3 = left], [4 = right], [0 = default case]
+var _last_dir: int = 0
 
 # PUBLIC VARIABLES
 # =======================================================================
@@ -24,8 +27,6 @@ func _char_controls(delta):
 
 # logic for movement animations
 func _char_controls_anims():
-	# holds last movement, to help display the right idle anim.
-	var last_dir: Vector2 = Vector2(0,0)
 	# round the x and y value to 1, 0, or -1
 	var rounded_dir: Vector2 = Vector2(round(direction.x), round(direction.y))
 	# handle the 0.7 and -0.7 special case
@@ -38,30 +39,48 @@ func _char_controls_anims():
 		# =======================================================================
 		Vector2(0,-1):
 			%AnimatedSprite2D.play("u_walk") # walk up
+			_last_dir = 1
 		Vector2(0,1):
 			%AnimatedSprite2D.play("d_walk") # walk down
+			_last_dir = 2
 		Vector2(1,0):
 			%AnimatedSprite2D.flip_h = true
 			%AnimatedSprite2D.play("s_walk") # walk right
+			_last_dir = 4
 		Vector2(-1,0):
 			%AnimatedSprite2D.flip_h = false
 			%AnimatedSprite2D.play("s_walk") # walk left
+			_last_dir = 3
 		# diag movements
 		# =======================================================================
 		Vector2(1,-1):
 			%AnimatedSprite2D.flip_h = true
 			%AnimatedSprite2D.play("s_walk") # walk right (up)
+			_last_dir = 4
 		Vector2(-1, -1):
 			%AnimatedSprite2D.flip_h = false
 			%AnimatedSprite2D.play("s_walk") # walk left (up)
+			_last_dir = 3
 		Vector2(1, 1):
 			%AnimatedSprite2D.flip_h = true
 			%AnimatedSprite2D.play("s_walk") # walk right (down)
+			_last_dir = 4
 		Vector2(-1, 1):
 			%AnimatedSprite2D.flip_h = false
 			%AnimatedSprite2D.play("s_walk") # walk left (down)
+			_last_dir = 3
 		_: # default case; idle anims.
-			%AnimatedSprite2D.play("d_idle")
+			match _last_dir:
+				1:
+					%AnimatedSprite2D.play("u_idle")
+				2: 
+					%AnimatedSprite2D.play("d_idle")
+				3:
+					%AnimatedSprite2D.flip_h = false
+					%AnimatedSprite2D.play("s_idle")
+				4:
+					%AnimatedSprite2D.flip_h = true
+					%AnimatedSprite2D.play("s_idle")
 	
 # READY & PROCESS
 # =======================================================================
